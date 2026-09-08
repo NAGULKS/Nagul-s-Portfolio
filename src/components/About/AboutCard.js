8c6a09488b157e1d34621795f7753b5173848ecb
NAGUL <90541258+NAGULKS@users.noreply.github.com>
2026-09-08 09:51:08 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,38 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Nagul K S </span>
-            from <span className="purple"> Salem, India.</span>
+          <p style={{ textAlign: "justify", lineHeight: "1.8" }}>
+            Hi Everyone, I am <span className="purple">Nagul K S</span> from{" "}
+            <span className="purple">Salem, India.</span>
             <br />
-            I am currently doing Intern as a Java Fullstack Trainee at Izeon Innovative pvt ltd.
+            I am a Computer Science and Engineering graduate from{" "}
+            <span className="purple">Kongu Engineering College</span>.
             <br />
-            I have completed BE Computer Science and Engineering in Kongu Engineering College.
+            I have professional experience as a{" "}
+            <span className="purple">Web Developer</span> at Optimus
+            Technocrates (India) Pvt. Ltd.
+            <br />
+            I also have experience as an{" "}
+            <span className="purple">IoT Developer Intern</span> at Magnertia
+            TBI@KEC, working with IoT hardware, AWS, and ThingSpeak.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            I am interested in software development, web technologies, IoT,
+            cloud integration, and building practical technology solutions.
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Exploring New Things
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Strive to build things that make a difference!"
           </p>
+
           <footer className="blockquote-footer">Nagul</footer>
         </blockquote>
       </Card.Body>

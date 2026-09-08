003101297195250ac463d766cc9ef1e8ec7385e6
NAGUL <90541258+NAGULKS@users.noreply.github.com>
2026-09-08 09:12:50 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/NAGUL.pdf";
+import pdf from "../../Assets/Nagul.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";

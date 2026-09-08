b593215d8239314014e40f9a8a201e6423276e86
NAGUL <90541258+NAGULKS@users.noreply.github.com>
2026-09-08 09:45:46 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,6 +11,7 @@ function About() {
   return (
     <Container fluid className="about-section">
       <Particle />
+
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col
@@ -24,25 +25,102 @@ function About() {
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
               Know Who <strong className="purple">I'M</strong>
             </h1>
+
             <Aboutcard />
           </Col>
+
           <Col
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
-            <img src={Nagul} alt="about" className="img-fluid" style={{height:"60%",width:"80%"}} />
+            <img
+              src={Nagul}
+              alt="about"
+              className="img-fluid"
+              style={{ height: "60%", width: "80%" }}
+            />
           </Col>
         </Row>
-        <h1 className="project-heading" style={{marginTop:"-200px"}}>
-          Professional <strong className="purple">Skillset </strong>
+
+        {/* Professional Experience */}
+        <h1
+          className="project-heading"
+          style={{ marginTop: "-80px", paddingBottom: "30px" }}
+        >
+          Professional <strong className="purple">Experience</strong>
+        </h1>
+
+        <Row style={{ justifyContent: "center", paddingBottom: "40px" }}>
+          <Col md={10}>
+            <div style={{ padding: "20px" }}>
+              <h3 style={{ fontSize: "1.3rem" }}>
+                Web Developer | Optimus Technocrates (India) Pvt. Ltd.
+              </h3>
+
+              <p>
+                <strong>Apr 2025 – Sep 2025</strong>
+              </p>
+
+              <p style={{ lineHeight: "1.7" }}>
+                Developed responsive web applications for client-based
+                projects.
+                <br />
+                Implemented web application requirements using frontend and
+                backend technologies.
+                <br />
+                Collaborated with the development team on application
+                development and project delivery.
+                <br />
+                Supported application testing, troubleshooting, and
+                implementation activities.
+              </p>
+            </div>
+
+            <div style={{ padding: "20px" }}>
+              <h3 style={{ fontSize: "1.3rem" }}>
+                IoT Developer Intern | Magnertia TBI@KEC
+              </h3>
+
+              <p>
+                <strong>Apr 2023 – Dec 2023</strong>
+              </p>
+
+              <p style={{ lineHeight: "1.7" }}>
+                Integrated IoT hardware with AWS and ThingSpeak for real-time
+                cloud monitoring.
+                <br />
+                Worked on IoT solutions involving hardware, software, and cloud
+                integration.
+                <br />
+                Supported real-time data monitoring through cloud-connected IoT
+                systems.
+                <br />
+                Collaborated with the development team to improve IoT system
+                connectivity and prototype development.
+              </p>
+            </div>
+          </Col>
+        </Row>
+
+        {/* Professional Skillset */}
+        <h1
+          className="project-heading"
+          style={{ marginTop: "0px" }}
+        >
+          Professional <strong className="purple">Skillset</strong>
         </h1>
 
         <Techstack />
 
-        <h1 className="project-heading"style={{marginTop:"0px"}}>
+        {/* Tools */}
+        <h1
+          className="project-heading"
+          style={{ marginTop: "0px" }}
+        >
           <strong className="purple">Tools</strong> I use
         </h1>
+
         <Toolstack />
 
         <Github />
